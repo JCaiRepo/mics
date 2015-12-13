5601f00132da0c6d1939ddf76f7e64ca18705d55
Jie Cai <jcai@turn.com>
2015-12-12 19:10:33 -0800

    update some minor bug for in.next to in.nextLine and condition check for boolean type

--- isomorphicString/src/com/personal/jcai/Main.java
+++ isomorphicString/src/com/personal/jcai/Main.java
@@ -9,17 +9,17 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter the 1st string");
-        String a = in.next();
+        String a = in.nextLine();
 
         System.out.println("Enter the 2nd string");
-        String b = in.next();
+        String b = in.nextLine();
 
         System.out.println("1st string is " + a + "; 2nd string is " + b);
 
         isomorphic isomorphicPair = new isomorphic(a, b);
         boolean result = isomorphicPair.check();
 
-        if(result == true) {
+        if(result) {
             System.out.println(a + " and " + b + " is isomorphic.");
         } else {
             System.out.println(a + " and " + b + " is not isomorphic.");
